cc0d40beac0594179f6a7788aa5a69f520344763
adferrin <adferrin@icloud.com>
2021-04-19 13:49:38 -0700

    functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,7 +5,91 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+let validPuzzle = 
+    "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 
+suite('Functional Tests', () => {
+    test("Solve a puzzle with valid puzzle string: POST request to /api/solve", function (done) {
+        chai
+        .request(server)
+        .post("/api/solve")
+        .send({ puzzle: validPuzzle })
+        .end(function (err, res) {
+            assert.equal(res.status, 200); 
+            let complete = 
+                "135762984946381257728459613694517832812936745357824196473298561581673429269145378";
+            assert.equal(res.body.solution, complete);
+            done();
+        });
+    });
+
+    test("Solve a puzzle with missing puzzle string: POST request to /api/solve", function (done) {
+        chai
+        .request(server)
+        .post("/api/solve")
+        .send({})
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.error, "Required field missing");
+            done();
+        });
+    });
+
+    test("Solve a puzzle with invaild characters: POST request to /api/solve", function (done) {
+        chai
+        .request(server)
+        .post("/api/solve")
+        .send({ 
+            puzzle:
+                "1.5..2.84..63.12.7.2..5...a.9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+        })
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.error, "Invalid characters in puzzle");
+            done();
+        });
+    });
+
+    test("Solve a puzzle wiht inccorect length: POST to /api/solve", function (done) {
+        chai
+        .request(server)
+        .post("/api/solve")
+        .send({
+            puzzle:
+                "1.5..2.84..63.12.7.2..5.......9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+        })
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.error, "Expected puzzle to be 81 characters long");
+            done();
+        });
+    });
+    
+    test("Solve a puzzle that cannot be solved: POST request to /api/solve", function (done) {
+        chai
+        .request(server)
+        .post("/api/solve")
+        .send({
+            puzzle:
+                "115..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+        })
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.error, "Puzzle cannot be solved");
+            done();
+        });
+    });
+
+    test("Check a puzzle placement with all fields: POST request to /api/check", function (done) {
+        chai
+        .request(server)
+        .post("/api/sovle")
+        .send({ puzzle: validPuzzle, coordinate: "A2", value: "3" })
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.valid, true);
+            done();
+        });
+    });
 });
 
